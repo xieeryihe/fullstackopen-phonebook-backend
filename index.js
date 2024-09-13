d1a954237bf785d57da01be4dd36868d0756fad0
xieeryihe <wangyingchuan@meituan.com>
2024-09-13 10:27:38 +0800

    Exercise 3.3 finish

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).send(`Error: Person not found.`)
+    }
+})
+
+app.get('/info', (request, response) => {
+    const requestTime = new Date();
+    // console.log(`Request received at: ${requestTime}`);
+    response.send(`
+        <div>Phonebook has info for ${persons.length} people.</div>
+        <div>${requestTime}</div>    
+    `)
+})
+
 const port = 3001
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
