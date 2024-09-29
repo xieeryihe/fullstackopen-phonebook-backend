b4975d333fcd03178b6051e63128e00ef9b45ff5
xieeryihe <wangyingchuan@meituan.com>
2024-09-29 17:02:16 +0800

    Exercise 3.8 finish

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body);
+});
+
+const customFormat = ':method :url :status :res[content-length] - :response-time ms :body';
+
+app.use(morgan(customFormat));
+
 
 persons = [
     {
@@ -63,7 +71,6 @@ app.delete('/api/persons/:id', ({ params: { id } }, response) => {
 
 app.post('/api/persons', (request, response) => {
     const person = request.body
-    console.log(person);
     
     if (!person.name) {
         return response.status(400).json({
